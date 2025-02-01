db6b78bc8787c6fae6f20c81cc807ad639c519bd
JulioVictorVL <116852038+JulioVictorVL@users.noreply.github.com>
2025-02-01 19:21:50 -0300

    Mods update

--- Update_Mods.bat
+++ Update_Mods.bat
@@ -1,11 +1,11 @@
 @echo off
-set "Repository_Path=%~dp0"
+set Repository_Path=%~dp0
 echo Repository_Path: %Repository_Path%
 %~d0
 cd %Repository_Path%
 
 
-set "Git_exe=%Repository_Path%Git-App\cmd\git"
+set Git_exe="%Repository_Path%Git-App\cmd\git"
 echo %Git_exe%
 %Git_exe% pull
 
@@ -16,6 +16,8 @@ call set "MOD_FOLDER=%MOD_FOLDER%"
 echo %MOD_FOLDER%
 dir "%MOD_FOLDER%"
 del /f /q /s %MOD_FOLDER%
+dir "%MOD_FOLDER%"
+xcopy "%Repository_Path%\mods" %MOD_FOLDER% /s /e /h
 dir "%MOD_FOLDER%"
 
 pause
