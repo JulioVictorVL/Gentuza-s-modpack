bde28a6894fa06119548bf36d2388c5b88930347
JulioVictorVL <116852038+JulioVictorVL@users.noreply.github.com>
2025-02-01 18:21:16 -0300

    Uptade_bat

---
+++ Update_Mods.bat
@@ -0,0 +1,19 @@
+@echo off
+set "Repository_Path=%~dp0"
+echo Repository_Path: %Repository_Path%
+%~d0
+cd %Repository_Path%
+
+
+set "Git_exe=%Repository_Path%Git-App\cmd\git"
+echo %Git_exe%
+%Git_exe% pull
+
+
+set /p MOD_FOLDER=<"%Repository_Path%\Mod_folder_path.txt"
+call set "MOD_FOLDER=%MOD_FOLDER%"
+
+echo %MOD_FOLDER%
+dir "%MOD_FOLDER%"
+
+pause
